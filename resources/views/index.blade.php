e17d10b92017de8bc09468f5a4a911a72ca931fe
Muhammad Faza <m.faza1998@gmail.com>
2020-05-23 23:05:01 +0700

    halaman awal dan upload gambar

---
+++ resources/views/index.blade.php
@@ -0,0 +1,229 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gallery</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="/img/fav.ico" type="image/x-icon">
+
+    <!-- css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+
+    <!-- javascript -->
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/script.js"></script>
+
+
+</head>
+<body>
+
+    <!-- Navigasi -->
+    <nav class="navbar navbar-expand-md fixed-top navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">Gallery</a>
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <!-- navbar kiri -->
+            <div class="collapse navbar-collapse justify-content-md-start" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                </ul>
+            </div>
+
+            <!-- navbar kanan -->
+            <div class="collapse navbar-collapse justify-content-md-end" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link nav-plus" data-toggle="modal" href="#upload"><strong>&plus;</strong></a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Username</a>
+                        <div class="dropdown-menu dropdown-menu-md-right">
+                        <a class="dropdown-item" href="#">Profile</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#">Settings</a>
+                        <a class="dropdown-item" href="#">Log out</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Akhir Navigasi -->
+
+
+    <!-- Konten -->
+    <section class="konten" id="konten">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <a href="#">
+                            <img src="img/thumbs/1.png" alt="1" class="card-img-top">
+                        </a>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h6 class="card-title">username</h6>
+                                </div>
+                                <div class="col-md-6">
+                                    <h6 class="figure-caption text-right"><em>16-02-1998</em></h6>
+                                </div>
+                            </div>
+
+                            <p class="card-text caption">
+                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <a href="#">
+                            <img src="img/thumbs/2.png" alt="2" class="card-img-top">
+                        </a>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h6 class="card-title">username</h6>
+                                </div>
+                                <div class="col-md-6">
+                                    <h6 class="figure-caption text-right"><em>16-02-1998</em></h6>
+                                </div>
+                            </div>
+
+                            <p class="card-text caption">
+                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <a href="#">
+                            <img src="img/thumbs/3.png" alt="3" class="card-img-top">
+                        </a>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h6 class="card-title">username</h6>
+                                </div>
+                                <div class="col-md-6">
+                                    <h6 class="figure-caption text-right"><em>16-02-1998</em></h6>
+                                </div>
+                            </div>
+
+                            <p class="card-text caption">
+                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <a href="#">
+                            <img src="img/thumbs/4.png" alt="4" class="card-img-top">
+                        </a>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h6 class="card-title">username</h6>
+                                </div>
+                                <div class="col-md-6">
+                                    <h6 class="figure-caption text-right"><em>16-02-1998</em></h6>
+                                </div>
+                            </div>
+
+                            <p class="card-text caption">
+                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <a href="#">
+                            <img src="img/thumbs/5.png" alt="5" class="card-img-top">
+                        </a>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h6 class="card-title">username</h6>
+                                </div>
+                                <div class="col-md-6">
+                                    <h6 class="figure-caption text-right"><em>16-02-1998</em></h6>
+                                </div>
+                            </div>
+
+                            <p class="card-text caption">
+                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <a href="#">
+                            <img src="img/profile-picture.png" alt="6" class="card-img-top">
+                        </a>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <h6 class="card-title">username</h6>
+                                </div>
+                                <div class="col-md-6">
+                                    <h6 class="figure-caption text-right"><em>16-02-1998</em></h6>
+                                </div>
+                            </div>
+
+                            <p class="card-text caption">
+                                Some quick example text to build on the card title and make up the bulk of the card's content.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Akhir Konten -->
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="upload" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="static">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+                <div class="modal-header bg-info text-light">
+                    <h5 class="modal-title" id="exampleModalLabel">Upload Picture</h5>
+                    <button type="button" class="close text-light" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <form action="" method="post" enctype="multipart/form-data">
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="upload-picture">Upload Picture</label>
+                            <input type="file" class="form-control-file" name="files[]" id="upload-picture">
+                        </div>
+                        <div class="form-group">
+                            <label for="upload-caption">Insert Caption</label>
+                            <textarea class="form-control" id="upload-caption" rows="3"></textarea>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                        <button type="submit" class="box__button btn btn-primary">Upload Picture</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- akhir modal -->
+
+</body>
+</html>
