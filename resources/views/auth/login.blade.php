8a45874bdf4d26fdaad27d8ee169d225cc35bf60
Muhammad Faza <m.faza1998@gmail.com>
2020-07-12 17:55:52 +0700

    efek login google

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,29 +26,35 @@
                 <form action="{{ route("login") }}" method="POST">
                     @csrf
                     <div class="form-group">
-                        <label for="username">Username</label>
-                        <input type="text" class="form-control @error('username') is-invalid @enderror" id="username" placeholder="Masukkan username" name="username" value="{{ old('username') }}">
-                        @error('username') 
+                        <div class="inputBox">
+                            <input type="text" class="form-control @error('username') is-invalid @enderror" id="username" name="username" value="{{ old('username') }}" onkeyup="this.setAttribute('value', this.value);">
+                            <label for="username">Username</label>
+                        </div>
+                        @error('username')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                         @enderror
                     </div>
                     <div class="form-group">
-                        <label for="sandi">Kata Sandi</label>
-                        <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" placeholder="Masukkan kata sandi">
-                        @error('password') 
+                        <div class="inputBox">
+                            <input type="password" class="form-control @error('password') is-invalid @enderror" id="password" name="password" onkeyup="this.setAttribute('value', this.value);" value="">
+                            <label for="sandi">Kata Sandi</label>
+                        </div>
+                        @error('password')
                         <span class="invalid-feedback" role="alert">
                             <strong>{{ $message }}</strong>
                         </span>
                         @enderror
                     </div>
-                    <div class="form-group form-check">
-                        <input type="checkbox" class="form-check-input" id="ingat_saya">
-                        <label class="form-check-label" for="ingat_saya">Ingat saya</label>
-                    </div>
                     <hr>
                     <div class="row">
+                        <div class="col">
+                            <div class="form-group form-check">
+                                <input type="checkbox" class="form-check-input" id="ingat_saya">
+                                <label class="form-check-label" for="ingat_saya">Ingat saya</label>
+                            </div>
+                        </div>
                         <div class="col">
                             <button type="submit" class="btn btn-primary float-right cf">Masuk</button>
                         </div>
